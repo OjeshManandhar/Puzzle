d20f5974bb2b82ebe4fe33e89aea76ef73041914
OjeshManandhar <gouenji.suyya.10@gmail.com>
2018-04-30 15:23:15 +0545

    Create config.h

---
+++ config.h
@@ -0,0 +1,10 @@
+#include <QBASIC_func_C.h>
+#include <basic_func.h>
+
+//Max values of row and col for scr_size 30x120
+#define MAX_ROW 14
+#define MAX_COL 24
+
+struct position_detail scr_size = {30, 120};
+
+unsigned short int default_row = 4, default_col = 4;
